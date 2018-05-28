c902ceee66f39ed052729e6b0142f26518130d8d
nrutemby <nil.rutembya@sogeti.com>
2018-05-28 10:28:14 +0200

    change ip adress server

--- app/src/main/java/com/example/android/models/DataModel.java
+++ app/src/main/java/com/example/android/models/DataModel.java
@@ -1,32 +1,23 @@
 package com.example.android.models;
-
-import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.content.Context;
 import android.util.Log;
-
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.android.activities.BuildConfig;
 import com.example.android.network.RequestQueueSingleton;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public abstract class DataModel extends ViewModel {
 
-    private static final String ip_address = "192.168.2.69";
+    private static final String ip_address = "192.168.2.118";
     public static String currentTableName;
     public static String currentColumnName;
     public static String currentNumberOfValues;
