1eaaf07e6e2b10e91adbfb93192b1fc3558e3c43
nrutemby <nil.rutembya@sogeti.com>
2018-06-08 14:13:31 +0200

    ~

---
+++ app/src/main/java/com/example/android/models/DataType.java
@@ -0,0 +1,10 @@
+package com.example.android.models;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by nrutemby on 07/06/2018.
+ */
+
+public class DataType extends RealmObject {
+}
---
+++ app/src/main/java/com/example/android/models/Scale.java
@@ -0,0 +1,8 @@
+package com.example.android.models;
+
+/**
+ * Created by nrutemby on 07/06/2018.
+ */
+
+public class Scale {
+}
