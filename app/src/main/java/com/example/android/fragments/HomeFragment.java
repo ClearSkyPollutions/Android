a7bf8d5bc7fb7f1bc85f09f2903c2af6b520687d
felixTamagny <felix.tamagny@sogeti.com>
2018-07-09 14:53:40 +0200

    Restructure init views,fixed bugs selection, rename vars

--- app/src/main/java/com/example/android/fragments/HomeFragment.java
+++ app/src/main/java/com/example/android/fragments/HomeFragment.java
@@ -14,6 +14,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -59,7 +60,7 @@ public class HomeFragment extends Fragment {
     private Button mButtonYear;
     private Button mButtonAddChart;
     private TextView mSelectedValueView;
-    private TextView mLabelView;
+    private TextView mSelectedDateView;
     private GridView mGridView;
     private ImageButton mImageButtonFront;
     private ImageButton mImageButtonBack;
@@ -72,7 +73,6 @@ public class HomeFragment extends Fragment {
     // Rotate View
     private AnimatorSet mSetRightOut;
     private AnimatorSet mSetLeftIn;
-    public boolean mIsBackCardVisible = false;
 
     private ChartItemAdapter chartItemAdapter;
 
@@ -108,18 +108,20 @@ public class HomeFragment extends Fragment {
             mCoverView.setVisibility(View.GONE);
             mCardAddChart.setVisibility(View.GONE);
             mButtonAddChart.setClickable(false);
-            if (!mIsBackCardVisible)
+            if (!chartItemAdapter.isBackCardVisible())
                 mSwipeRefreshLayout.setEnabled(true);
         });
 
         // Functions called when a chart is clicked on
         mGridView.setOnItemClickListener((parent, view, position, id) -> {
-
             mChartViewDialog.setVisibility(View.VISIBLE);
             mCoverView.setVisibility(View.VISIBLE);
 
+            //Reset selected position
+            mChartDialog.highlightValues(null);
+
             // Get the correct LiveData(pm10, pm25...) to fill chartDialog with data
-            chartItemAdapter.getItem(position).observe((LifecycleOwner) getContext(), newChart -> {
+            mDataModel.chartList.get(position).observe((LifecycleOwner) getContext(), newChart -> {
                 mChartHelper.reset(mChartDialog);
                 for (int index = 0; index < newChart.getXAxis().size(); index++) {
                     String dateString = ChartHelper.getStringDate(newChart.getXAxis().get(index), "");
@@ -128,7 +130,6 @@ public class HomeFragment extends Fragment {
                     Float[] entry = new Float[]{ts_f, value};
                     mChartHelper.addEntry(mChartDialog, entry, newChart.getColor(), true);
                 }
-                mChartHelper.getSelected().setValue(mChartHelper.getEntries().size() - 1);
             });
 
             //Change the buttons event according to dataType
@@ -153,7 +154,7 @@ public class HomeFragment extends Fragment {
                 // -1 for error in selection, null if mutableLiveData not set
                 if (selected == null || selected == -1) {
                     mSelectedValueView.setText("");
-                    mLabelView.setText("");
+                    mSelectedDateView.setText("");
                 } else {
                     Chart ch = mDataModel.chartList.get(position).getValue();
 
@@ -164,7 +165,7 @@ public class HomeFragment extends Fragment {
                     String dateString = ChartHelper.getStringDate(date, ch.getScale());
 
                     mSelectedValueView.setText(val + " " + ch.getUnit());
-                    mLabelView.setText(dateString);
+                    mSelectedDateView.setText(dateString);
                 }
             });
 
@@ -172,8 +173,8 @@ public class HomeFragment extends Fragment {
         });
 
         // Functions Onclick on ImageButton
-        mImageButtonFront.setOnClickListener(this::flipCard);
-        mImageButtonBack.setOnClickListener(this::flipCard);
+        mImageButtonFront.setOnClickListener(v->flipCard());
+        mImageButtonBack.setOnClickListener(v->flipCard());
 
         mImageButtonAddChart.setOnClickListener(v -> {
             mCardAddChart.setVisibility(View.VISIBLE);
@@ -186,7 +187,7 @@ public class HomeFragment extends Fragment {
             boolean existing = false;
             int i = 0;
             while (!existing && i < mDataModel.chartList.size()) {
-                existing = chartItemAdapter.getItem(i).getValue().getType().equals(mSpinnerDataType.getSelectedItem());
+                existing = mDataModel.chartList.get(i).getValue().getType().equals(mSpinnerDataType.getSelectedItem());
                 i++;
             }
             if (!existing) {
@@ -242,100 +243,90 @@ public class HomeFragment extends Fragment {
                 mSpinnerDataUnits.setAdapter(listUnitAdapter);
             }
         });
+
         return mRootView;
     }
 
 
-    private void initViews() {
-        // Init charts
-        int chartTextColor = getResources().getColor(R.color.primaryTextColor);
-        int chartBackgroundColor = Color.WHITE;
-
-        // Create the adapter to convert the array to views
+    private void initAdapter() {
+        // Create the adapter to convert the array of pollutant into views, bind it to gridview
         chartItemAdapter = new ChartItemAdapter(getActivity(), mDataModel.chartList, mChartHelper);
-
-
-        chartItemAdapter.setIsBackCardVisible(false);
-        // Attach the adapter to the GridView
         mGridView = mRootView.findViewById(R.id.chartlist);
         mGridView.setAdapter(chartItemAdapter);
+    }
 
-        mChartDialog = mRootView.findViewById(R.id.lineChartDialog);
-        mChartHelper.initChartDialog(mChartDialog, chartBackgroundColor, chartTextColor);
-        mChartViewDialog = mRootView.findViewById(R.id.viewDialog);
-
-        // Init CardView
-        mCardCitiesFront = mRootView.findViewById(R.id.CardFront);
-        mCardCitiesBack = mRootView.findViewById(R.id.CardBack);
-        mCardAddChart = mRootView.findViewById(R.id.addElement);
-
-        //For animation change the distance
-        changeCameraDistance();
+    private void initDialogChart() {
+        int chartTextColor = getResources().getColor(R.color.primaryTextColor);
+        int chartBackgroundColor = Color.WHITE;
 
         //Init buttons
         mButtonDay = mRootView.findViewById(R.id.day_bt);
         mButtonMonth = mRootView.findViewById(R.id.month_bt);
         mButtonYear = mRootView.findViewById(R.id.year_bt);
-        mButtonAddChart = mRootView.findViewById(R.id.buttonAddElement);
 
         // Init popup
         mCoverView = mRootView.findViewById(R.id.cover);
-        mLabelView = mRootView.findViewById(R.id.labelData);
-        mSelectedValueView = mRootView.findViewById(R.id.data);
+        mSelectedDateView = mRootView.findViewById(R.id.SelectedDate);
+        mSelectedValueView = mRootView.findViewById(R.id.SelectedValue);
 
-        // Init ImageButton
-        mImageButtonFront = mRootView.findViewById(R.id.buttonEditFront);
-        mImageButtonBack = mRootView.findViewById(R.id.buttonEditBack);
-        mImageButtonAddChart = mRootView.findViewById(R.id.buttonAddBack);
+        //init chart
+        mChartDialog = mRootView.findViewById(R.id.lineChartDialog);
+        mChartHelper.initChartDialog(mChartDialog, chartBackgroundColor, chartTextColor);
+        mChartViewDialog = mRootView.findViewById(R.id.viewDialog);
+    }
+
+    private void initBackViews() {
+        // Dialog to add new pollutants
+        mButtonAddChart = mRootView.findViewById(R.id.buttonAddElement);
+        mCardAddChart = mRootView.findViewById(R.id.CardDialogNewChart);
 
         // Init Spinner
         mSpinnerNameSensors = mRootView.findViewById(R.id.SPnameSensors);
         mSpinnerDataType = mRootView.findViewById(R.id.SPdataType);
         mSpinnerDataUnits = mRootView.findViewById(R.id.SPdataUnits);
 
-        // Init SwipeRefreshLayout
-        mSwipeRefreshLayout = mRootView.findViewById(R.id.swiperefresh);
+        // Init ImageButton
+        mImageButtonFront = mRootView.findViewById(R.id.buttonEditFront);
+        mImageButtonBack = mRootView.findViewById(R.id.buttonEditBack);
+        mImageButtonAddChart = mRootView.findViewById(R.id.buttonAddBack);
+    }
+
+    private void initViews() {
+
+        initAdapter();
+        initDialogChart();
+        initBackViews();
+
+        // Init summary cardViews
+        mCardCitiesFront = mRootView.findViewById(R.id.CardFront);
+        mCardCitiesBack = mRootView.findViewById(R.id.CardBack);
 
         // Init Animation
         mSetRightOut = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), R.animator.out_animation);
         mSetLeftIn = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), R.animator.in_animation);
+        //For animation change the distance
+        changeCameraDistance();
+
+        // Init SwipeRefreshLayout
+        mSwipeRefreshLayout = mRootView.findViewById(R.id.swiperefresh);
     }
 
-    public void flipCard(View view) {
-        if (mIsBackCardVisible) {
-            mSetRightOut.setTarget(mCardCitiesBack);
-            mSetLeftIn.setTarget(mCardCitiesFront);
-            for (int i = 0; i < chartItemAdapter.mChartCardFront.size(); i++) {
-                chartItemAdapter.mChartCardFront.get(i).setVisibility(View.VISIBLE);
-                chartItemAdapter.mChartCardBack.get(i).setVisibility(View.INVISIBLE);
-            }
-            mSetRightOut.start();
-            mSetLeftIn.start();
-            mImageButtonFront.setClickable(true);
-            mImageButtonBack.setClickable(false);
-            mButtonAddChart.setClickable(false);
-            mImageButtonAddChart.setClickable(false);
-            mCardAddChart.setVisibility(View.GONE);
-            mSwipeRefreshLayout.setEnabled(true);
-            mIsBackCardVisible = false;
-            chartItemAdapter.setIsBackCardVisible(false);
-        } else {
-            mSetRightOut.setTarget(mCardCitiesFront);
-            mSetLeftIn.setTarget(mCardCitiesBack);
-            for (int i = 0; i < chartItemAdapter.mChartCardFront.size(); i++) {
-                chartItemAdapter.mChartCardFront.get(i).setVisibility(View.INVISIBLE);
-                chartItemAdapter.mChartCardBack.get(i).setVisibility(View.VISIBLE);
-            }
-            mSetRightOut.start();
-            mSetLeftIn.start();
-            mImageButtonFront.setClickable(false);
-            mImageButtonBack.setClickable(true);
-            mButtonAddChart.setClickable(true);
-            mImageButtonAddChart.setClickable(true);
-            mSwipeRefreshLayout.setEnabled(false);
-            mIsBackCardVisible = true;
-            chartItemAdapter.setIsBackCardVisible(true);
-        }
+    public void flipCard() {
+        boolean flipToFrontCard = chartItemAdapter.isBackCardVisible();
+
+        mSetRightOut.setTarget(flipToFrontCard ? mCardCitiesBack:mCardCitiesFront);
+        mSetLeftIn.setTarget(flipToFrontCard ? mCardCitiesFront:mCardCitiesBack);
+        mSetRightOut.start();
+        mSetLeftIn.start();
+        mImageButtonFront.setClickable(flipToFrontCard);
+        mImageButtonBack.setClickable(!flipToFrontCard);
+        mButtonAddChart.setClickable(!flipToFrontCard);
+        mImageButtonAddChart.setClickable(!flipToFrontCard);
+        mSwipeRefreshLayout.setEnabled(flipToFrontCard);
+
+        chartItemAdapter.setIsBackCardVisible(!flipToFrontCard);
+
+        chartItemAdapter.notifyDataSetChanged();
     }
 
     private void changeCameraDistance() {
@@ -344,5 +335,4 @@ public class HomeFragment extends Fragment {
         mCardCitiesFront.setCameraDistance(scale);
         mCardCitiesBack.setCameraDistance(scale);
     }
-
 }
